086259ec3f608e0a020269e418cbd0df5f334978
Mosh <sherlockholmes>
2024-01-08 16:19:02 +0700

    Push code

---
+++ LORE-TSR/src/scripts/infer/demo_wired.sh
@@ -0,0 +1,23 @@
+python demo.py ctdet \
+        --dataset table \
+        --demo ../input_images/wired \
+        --demo_name demo_wired \
+        --debug 1 \
+        --arch dla_34 \
+        --K 3000 \
+        --MK 5000 \
+        --tsfm_layers 4 \
+        --stacking_layers 4 \
+        --gpus 0\
+        --wiz_4ps \
+        --wiz_detect \
+        --wiz_rev \
+        --wiz_stacking \
+        --convert_onnx 0 \
+        --vis_thresh_corner 0.3 \
+        --vis_thresh 0.20 \
+        --scores_thresh 0.2 \
+        --nms \
+        --demo_dir ../visualization_wired/ \
+        --load_model ../dir_of_ckpt/ckpt_wtw/model_best.pth \
+        --load_processor ../dir_of_ckpt/ckpt_wtw/processor_best.pth
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ source ~/.bashrc
 conda create --name Lore python=3.7
 conda activate Lore
 pip install -r requirements.txt
-pip3 install torch==1.8.2 torchvision==0.9.2 torchaudio==0.8.2 --extra-index-url https://download.pytorch.org/whl/lts/1.8/cu102
+pip install torch==1.6.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 chmod +x  *.sh
 cd lib/models/networsk/DCNv2
 ./make.sh
